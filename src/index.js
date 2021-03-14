aa349ff6a1ce9963d1909693986110996604ee7d
pyrus-vagus <grushadaria@gmail.com>
2021-03-14 03:23:32 +0300

    feat: implement function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
+exports.min = function min(array) {
+    return typeof array === "undefined" || array.length === 0
+        ? 0
+        : Math.min(...array);
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    return typeof array === "undefined" || array.length === 0
+        ? 0
+        : Math.max(...array);
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    return typeof array === "undefined" || array.length === 0
+        ? 0
+        : array.reduce((a, b) => a + b, 0) / array.length;
+};
